f27c3b829a7ae106b95e42bbf606710ec04dccf3
Lakshya2510 <74653849+Lakshya2510@users.noreply.github.com>
2021-05-26 17:50:10 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,30 @@
+const accessCode1 = "TIGER";
+const accessCode2 = "TELEPHONE";
+const accessCode3 = "BROTHER";
+
+function clues() {
+    
+    // Clue for Q-1
+
+    fill("white")
+    textSize(15)
+    text("G I R E T", 100,50)
+    fill("lightblue")
+    text("Hint: It is an animal !!", 100,70)
+
+    // Clue for Q-2
+
+    fill("white")
+    textSize(15)
+    text("L P E T H E O E N", 700, 150)
+    fill("lightblue")
+    text("Hint: It is a communication device !!", 700, 170)
+    
+    // Clue for Q-3
+
+    fill("white")
+    textSize(15)
+    text("O H B R R E T", 100, 300)
+    fill("lightblue")
+    text("Hint: It is a relation !!", 100, 320)
+}
